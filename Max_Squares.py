df84cd241912516fe131cefe06788409ced8b76c
Vrundaa <41923217+Vrundaa@users.noreply.github.com>
2019-02-04 10:17:02 +0530

    Add files via upload

---
+++ Max_Squares.py
@@ -0,0 +1,34 @@
+def get_maxSquare(length,width,count):
+
+    if length==width:
+
+        count=count+1
+
+        return count
+
+    else :
+
+        if length>width:
+
+            length=length-width
+
+            count=count+1
+
+            count= get_maxSquare(length,width,count)
+
+        elif width>length:
+
+            width=width-length
+
+            count=count+1
+
+            count=get_maxSquare(length,width,count)
+
+    return count
+
+
+local_count=0
+
+max_Squares=get_maxSquare(7,4,local_count)
+
+print(max_Squares)
